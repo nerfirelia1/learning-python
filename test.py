c11853a399f135daf9f69258da580573da0500eb
nerfirelia1 <adanex742@gmail.com>
2021-11-01 00:54:31 +0100

    Fixed test.py

--- test.py
+++ test.py
@@ -7,12 +7,12 @@ name = "John"
 hello("Jasiu")
 
 print("Hello {}".format(name))
-ALE WAŁEK JUZ NIE DZIAŁA ELO
 
 print("Hello " + name)
 
 
-def awesome_func(x,y):
+def awesome_func(x, y):
     return x**y
 
-print(awesome_func(2,4))
+
+print(awesome_func(2, 4))
